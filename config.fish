7dd732771184048e117239ba0615082e7d210dea
Luis Mauro Martinez Baez <mauromb3@gmail.com>
2020-05-27 17:50:02 -0500

    updates pyenv interactive

--- config.fish
+++ config.fish
@@ -1,8 +1,9 @@
 # pyenv
 set -x PYENV_ROOT $HOME/.pyenv
 set -x PATH $PYENV_ROOT/bin $PATH
-status --is-interactive; and . (pyenv init -|psub)
-status --is-interactive; and . (pyenv virtualenv-init -|psub)
+
+status is-interactive; and pyenv init - | source
+status is-interactive; and pyenv virtualenv-init - | source
 
 # GOPATH
 
